6665b407488a2dd619f3d2f616d2bc1f1df95b75
Will Vaughn <willieviseoae@gmail.com>
2016-03-27 23:41:42 -0700

    cipher

--- chapter9/cipher.hs
+++ chapter9/cipher.hs
@@ -1,3 +1,31 @@
 module Cipher where
 
 import Data.Char
+
+alphaRange :: Int
+alphaRange = 58
+
+lowerBound :: Int
+lowerBound = ord 'A'
+
+upperBound :: Int
+upperBound = ord 'z'
+
+encodeChar :: Int -> Char -> Char
+encodeChar shift char
+  | shiftedCharCode > upperBound = chr (shiftedCharCode - alphaRange)
+  | shiftedCharCode < lowerBound = chr (shiftedCharCode + alphaRange)
+  | otherwise = chr shiftedCharCode
+  where shiftMod = mod shift alphaRange
+        charCode = ord char
+        shiftedCharCode = charCode + shiftMod
+
+
+decodeChar :: Int -> Char -> Char
+decodeChar = encodeChar . negate
+
+caesar :: Int -> String -> String
+caesar shift = map (encodeChar shift)
+
+unCaesar :: Int -> String -> String
+unCaesar shift = map (decodeChar shift)
