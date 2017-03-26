67e645eaf9dfce5421e2be12f3b2c2344bc4db14
Will Vaughn <willieviseoae@gmail.com>
2017-03-25 17:36:16 -0700

    chapter2 newlines and some linting

--- chapter2/FunctionWithLet.hs
+++ chapter2/FunctionWithLet.hs
@@ -1,5 +1,6 @@
 -- FunctionWithLet.hs
 module FunctionWithLet where
 
+printInc :: (Num a, Show a) => a -> IO ()
 printInc n = let plusTwo = n + 2
              in print plusTwo
--- chapter2/FunctionWithWhere.hs
+++ chapter2/FunctionWithWhere.hs
@@ -1,5 +1,6 @@
 -- FunctionWithWhere.hs
 module FunctionWithWhere where
 
+printInc :: (Num a, Show a) => a -> IO ()
 printInc n = print plusTwo
   where plusTwo = n + 2
